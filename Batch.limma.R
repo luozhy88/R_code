117c9c6014dfcd7760085e30ab2d0b4d6d75bed0
luozhy88 <94740863+luozhy88@users.noreply.github.com>
2024-10-23 15:57:11 +0800

    Create Batch.limma.R

---
+++ Batch.limma.R
@@ -0,0 +1,29 @@
+
+
+# 
+# # 使用limma包
+# library(limma)
+# batch_corrected_data <- removeBatchEffect(data_matrix, batch=batch_info)
+
+
+
+ngenes <- 10
+nsamples <- 8
+y <- matrix(rnorm(ngenes*nsamples),ngenes,nsamples)
+group <- factor(c("A","A","A","A","B","B","B","B"))
+batch <- factor(c(1,1,2,2,1,1,2,2))
+colnames(y) <- paste(group,batch,sep=".")
+y[,batch==2] <- y[,batch==2] + 5
+y[,group=="B"] <- y[,group=="B"] + 1
+
+# trace(removeBatchEffect,edit=TRUE)
+y.corrected <- removeBatchEffect(y, batch=batch)
+
+
+oldpar <- par(mfrow=c(1,2))
+plotMDS(y,main="Original")
+plotMDS(y.corrected,main="Batch corrected")
+par(oldpar)
+
+
+
